cd837fe37b3e7c9104d75809cc1eb9856287d633
CesarRondo <cesar.rondorodriguez@ucdconnect.ie>
2024-02-22 13:12:01 +0000

    made main function (in test class) call the interface

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,9 @@
+import javafx.application.Application;
 
-
-public class Test{
+public class Test {
     public static void main(String[] args)
     {
+        Application.launch(Interface.class,args);
         System.out.println("Test");
     }
 
