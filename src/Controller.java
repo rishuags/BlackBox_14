e8fdd03df2c493dd9e4674684941f5545512924a
CesarRondo <cesar.rondorodriguez@ucdconnect.ie>
2024-02-22 15:52:51 +0000

    deleted test class

--- src/Controller.java
+++ src/Controller.java
@@ -1,7 +1,10 @@
 import java.util.Map;
-
+import javafx.application.Application;
 public class Controller {
     public static void main(String[] args) {
+        Application.launch(Interface.class,args);
+
+
         Board board = new Board();
 
         Configuration c = new Configuration();
