658977d7db8023f218fa0f0aa3c1429b3d56aa55
Rishu <agrawalrishu8@gmail.com>
2024-03-07 13:04:44 +0000

    before laser class addition

--- src/Jar_Main.java
+++ src/Jar_Main.java
@@ -10,15 +10,13 @@ public class Jar_Main {
         Configuration.generateBoard();
 
 
-        for(Map.Entry<Integer, Gate> d : config.getGateMap().entrySet()){
-            //System.out.println(d);
-        }
-
-        for(Map.Entry<String, Tile> d : config.getCoordMap().entrySet()){
-            System.out.println(d.getKey());
-        }
-
-
+//        for(Map.Entry<Integer, Gate> d : config.getGateMap().entrySet()){
+//            //System.out.println(d);
+//        }
+//
+//        for(Map.Entry<String, Tile> d : config.getCoordMap().entrySet()){
+//            System.out.println(d.getKey());
+//        }
 
         Controller.main(args);
     }
