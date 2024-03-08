d7cdc69c108ee193b18350a6b37aa9a951fa777f
rishuags <107926886+rishuags@users.noreply.github.com>
2024-03-08 13:28:41 +0000

    Update ConfigurationTest.java

--- src/ConfigurationTest.java
+++ src/ConfigurationTest.java
@@ -1,19 +1,21 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedHashSet;
 import java.util.Map;
-
+import java.util.Set;
 
 
 class ConfigurationTest {
 
 
     @Test
-    public void testInitGateMap() {
+    public void testInitGateMapCoordinate() {
         Configuration.initGateMap();
         Map<Integer, Gate> gateMap = Configuration.getGateMap();
-
 
         //Testing Size
         assertEquals(54, gateMap.size());
@@ -29,7 +31,6 @@ class ConfigurationTest {
         assertEquals("-61", gateMap.get(8).getCoordinate().getKey());
         assertEquals("-80", gateMap.get(9).getCoordinate().getKey());
 
-
         assertEquals("-80", gateMap.get(10).getCoordinate().getKey());
         assertEquals("-80", gateMap.get(11).getCoordinate().getKey());
         assertEquals("-8-1", gateMap.get(12).getCoordinate().getKey());
@@ -39,7 +40,6 @@ class ConfigurationTest {
         assertEquals("-8-3", gateMap.get(16).getCoordinate().getKey());
         assertEquals("-8-3", gateMap.get(17).getCoordinate().getKey());
         assertEquals("-8-4", gateMap.get(18).getCoordinate().getKey());
-
 
         assertEquals("-8-4", gateMap.get(19).getCoordinate().getKey());
         assertEquals("-8-4", gateMap.get(20).getCoordinate().getKey());
@@ -51,7 +51,6 @@ class ConfigurationTest {
         assertEquals("-2-4", gateMap.get(26).getCoordinate().getKey());
         assertEquals("0-4", gateMap.get(27).getCoordinate().getKey());
 
-
         assertEquals("0-4", gateMap.get(28).getCoordinate().getKey());
         assertEquals("0-4", gateMap.get(29).getCoordinate().getKey());
         assertEquals("2-3", gateMap.get(30).getCoordinate().getKey());
@@ -61,9 +60,6 @@ class ConfigurationTest {
         assertEquals("6-1", gateMap.get(34).getCoordinate().getKey());
         assertEquals("6-1", gateMap.get(35).getCoordinate().getKey());
         assertEquals("80", gateMap.get(36).getCoordinate().getKey());
-
-
-
 
         assertEquals("80", gateMap.get(37).getCoordinate().getKey());
         assertEquals("80", gateMap.get(38).getCoordinate().getKey());
@@ -87,11 +83,95 @@ class ConfigurationTest {
         assertEquals("04", gateMap.get(54).getCoordinate().getKey());
 
 
-
-        // You can write more specific assertions based on your requirements
-        // For example, you could check if the gates are properly initialized
         assertNotNull(gateMap.get(1)); // Check if the gate with key 1 exists
-        // Add more assertions as needed
+
     }
+
+    @Test
+    public void testInitGateMapDirection() {
+        Configuration.initGateMap();
+        Map<Integer, Gate> gateMap = Configuration.getGateMap();
+
+        //Testing Size
+        assertEquals(54, gateMap.size());
+
+        //Testing ID - Initial Direction
+        assertEquals(Direction.SOUTH_EAST, gateMap.get(1).getDirection());
+        assertEquals(Direction.EAST, gateMap.get(2).getDirection());
+        assertEquals(Direction.SOUTH_EAST, gateMap.get(3).getDirection());
+        assertEquals(Direction.EAST, gateMap.get(4).getDirection());
+        assertEquals(Direction.SOUTH_EAST, gateMap.get(5).getDirection());
+        assertEquals(Direction.EAST, gateMap.get(6).getDirection());
+        assertEquals(Direction.SOUTH_EAST, gateMap.get(7).getDirection());
+        assertEquals(Direction.EAST, gateMap.get(8).getDirection());
+        assertEquals(Direction.SOUTH_EAST, gateMap.get(9).getDirection());
+
+        assertEquals(Direction.EAST, gateMap.get(10).getDirection());
+        assertEquals(Direction.NORTH_EAST, gateMap.get(11).getDirection());
+        assertEquals(Direction.EAST, gateMap.get(12).getDirection());
+        assertEquals(Direction.NORTH_EAST, gateMap.get(13).getDirection());
+        assertEquals(Direction.EAST, gateMap.get(14).getDirection());
+        assertEquals(Direction.NORTH_EAST, gateMap.get(15).getDirection());
+        assertEquals(Direction.EAST, gateMap.get(16).getDirection());
+        assertEquals(Direction.NORTH_EAST, gateMap.get(17).getDirection());
+        assertEquals(Direction.EAST, gateMap.get(18).getDirection());
+
+        assertEquals(Direction.NORTH_EAST, gateMap.get(19).getDirection());
+        assertEquals(Direction.NORTH_WEST, gateMap.get(20).getDirection());
+        assertEquals(Direction.NORTH_EAST, gateMap.get(21).getDirection());
+        assertEquals(Direction.NORTH_WEST, gateMap.get(22).getDirection());
+        assertEquals(Direction.NORTH_EAST, gateMap.get(23).getDirection());
+        assertEquals(Direction.NORTH_WEST, gateMap.get(24).getDirection());
+        assertEquals(Direction.NORTH_EAST, gateMap.get(25).getDirection());
+        assertEquals(Direction.NORTH_WEST, gateMap.get(26).getDirection());
+        assertEquals(Direction.NORTH_EAST, gateMap.get(27).getDirection());
+
+        assertEquals(Direction.NORTH_WEST, gateMap.get(28).getDirection());
+        assertEquals(Direction.WEST, gateMap.get(29).getDirection());
+        assertEquals(Direction.NORTH_WEST, gateMap.get(30).getDirection());
+        assertEquals(Direction.WEST, gateMap.get(31).getDirection());
+        assertEquals(Direction.NORTH_WEST, gateMap.get(32).getDirection());
+        assertEquals(Direction.WEST, gateMap.get(33).getDirection());
+        assertEquals(Direction.NORTH_WEST, gateMap.get(34).getDirection());
+        assertEquals(Direction.WEST, gateMap.get(35).getDirection());
+        assertEquals(Direction.NORTH_WEST, gateMap.get(36).getDirection());
+
+        assertEquals(Direction.WEST, gateMap.get(37).getDirection());
+        assertEquals(Direction.SOUTH_WEST, gateMap.get(38).getDirection());
+        assertEquals(Direction.WEST, gateMap.get(39).getDirection());
+        assertEquals(Direction.SOUTH_WEST, gateMap.get(40).getDirection());
+        assertEquals(Direction.WEST, gateMap.get(41).getDirection());
+        assertEquals(Direction.SOUTH_WEST, gateMap.get(42).getDirection());
+        assertEquals(Direction.WEST, gateMap.get(43).getDirection());
+        assertEquals(Direction.SOUTH_WEST, gateMap.get(44).getDirection());
+        assertEquals(Direction.WEST, gateMap.get(45).getDirection());
+
+        assertEquals(Direction.SOUTH_WEST, gateMap.get(46).getDirection());
+        assertEquals(Direction.SOUTH_EAST, gateMap.get(47).getDirection());
+        assertEquals(Direction.SOUTH_WEST, gateMap.get(48).getDirection());
+        assertEquals(Direction.SOUTH_EAST, gateMap.get(49).getDirection());
+        assertEquals(Direction.SOUTH_WEST, gateMap.get(50).getDirection());
+        assertEquals(Direction.SOUTH_EAST, gateMap.get(51).getDirection());
+        assertEquals(Direction.SOUTH_WEST, gateMap.get(52).getDirection());
+        assertEquals(Direction.SOUTH_EAST, gateMap.get(53).getDirection());
+        assertEquals(Direction.SOUTH_WEST, gateMap.get(54).getDirection());
+
+        assertNotNull(gateMap.get(1)); // Check if the gate with key 1 exists
+
+    }
+
+    @Test
+    public void testGenerateBoard(){
+        Configuration newConfig = new Configuration();
+
+        newConfig.generateBoard();
+
+        Map<String, Tile> coordTileMap = newConfig.getCoordMap();
+
+        assertNotNull(coordTileMap);
+        assertEquals(61, coordTileMap.size());
+
+    }
+
 
 }
